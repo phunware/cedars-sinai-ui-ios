ea97dd9729bcd421f5d7cf3c231c076c4ad9a658
Tomás Figueroa <tfigueroa@phunware.com>
2018-02-07 12:06:50 -0600

    Updated framework

--- Framework/CedarsSinai.framework/Headers/CedarsSinai.h
+++ Framework/CedarsSinai.framework/Headers/CedarsSinai.h
@@ -7,7 +7,7 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for CedarSinai.
+//! Project version number for CearSinai.
 FOUNDATION_EXPORT double CedarSinaiVersionNumber;
 
 //! Project version string for CedarSinai.
